7ed278bab00fc4b0e0a914ebb94673b720351202
wangyuteng02 <wangyuteng02@meituan.com>
2018-11-08 11:03:36 +0800

    初始化提交

---
+++ main/src/dev_20181108/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * @description:
+ * @author: wangyuteng02
+ * @date: 2018/11/8
+ * @time: 上午11:02
+ * Copyright (C) 2018 Meituan
+ * All rights reserved
+ */
+
+package dev_20181108;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
